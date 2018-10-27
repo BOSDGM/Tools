0a98a4e0fb96137c35093338b92354f2a14c1eed
bosdgm <565956231@qq.com>
2018-10-27 17:07:46 +0800

    parse

--- 01-remote/01-auto_ssh/no_pwd_remote_for_ssh.py
+++ 01-remote/01-auto_ssh/no_pwd_remote_for_ssh.py
@@ -2,9 +2,9 @@
 # -*- coding:utf-8 -*-
 # Power by HPCM   2018-10-10 15:05:01
 
-name=HPCM
-ip=192.168.190.1
+current_name=HPCM
+current_ip=192.168.190.1
 remote_path=C:/Users/w5659/.ssh
 
 ssh-keygen -t rsa &&/
-scp $HOME/.ssh/id_rsa.pub $name@$ip:$remote_path/authorized_keys
+scp $HOME/.ssh/id_rsa.pub $current_name@$current_ip:$remote_path/authorized_keys
